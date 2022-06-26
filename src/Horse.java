2d57a325c9e794b601e1b19754c5127c190e680d
maevski.ed <maevski.ed@gmail.com>
2022-06-26 13:45:15 +0300

    add class Horse

--- src/Horse.java
+++ src/Horse.java
@@ -10,11 +10,15 @@ public class Horse extends ChessPiece {
 
     @Override
     boolean canMoveToPosition(ChessBoard chessBoard, int line, int column, int toLine, int toColumn) {
-        return false;
+        if (line == toLine || column == toColumn || toLine < 0 || toLine > 7 ||
+                toColumn < 0 || toColumn > 7) return false;
+        if ((Math.abs(line - toLine) == 2 && Math.abs(column - toColumn) == 1) ||
+                (Math.abs(column - toColumn) == 2 && Math.abs(line - toLine) == 1)) return true;
+        else return false;
     }
 
     @Override
     String getSymbol() {
-        return null;
+        return "H";
     }
 }
